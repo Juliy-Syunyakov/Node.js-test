0a58d3066982ea364a5f225cdd3462ae349d632d
Juliy-Syunyakov <usunakov@gmail.com>
2022-02-03 17:22:28 +0300

    Create types.js

---
+++ lessons/types.js
@@ -0,0 +1,20 @@
+/*null-нет значения
+ undefined-не обьявлена переменная
+ boolean - TRUE/FALSE
+ number-число
+ string - строка
+ object-обьект (не приметивный)
+ symbol - символ
+*/
+/*console.log(typeof 0)
+console.log(typeof true)
+console.log(typeof "text")
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof function fn1(){})
+console.log(typeof NaN)*/
+console.log(1 + "2")
+console.log(1 - "2")
+console.log("3" * "2")
+console.log(18 + 2 + "2")
+console.log("hello" + 4 + 8)
