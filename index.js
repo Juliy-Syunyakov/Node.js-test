9a1068013a00ba4d2447ad76e0de27776c969dd0
Juliy-Syunyakov <usunakov@gmail.com>
2022-02-03 17:22:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-/*console.log("Hello world")
-console.log(" Hello world2")*/
-let hello = "hello"
-let world = "world328"
-console.log(`${hello} ${world}`)
-document.write(hello)
+const express = require('express')
+const app = express()
+const port = 3000
+//get: запрос на получение данных
+//post: запрос на передачу данных
+// req: данные от запроса
+//res:обратная ссылка чтобы ответить на запрос
+app.get('/', function (req,res){
+    console.log('hello')
+    res.send('text')
+})
+app.get('/account', function (req,res){
+    console.log('account')
+    res.send('<h1>account</h1>')
+})
+app.listen(port,()=>{
+    console.log(`app listen http://localhost:${port}`)
+})
